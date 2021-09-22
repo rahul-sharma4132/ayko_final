6bb00241381e125ae949119a052370a06d6fcd94
rahul_ayko <rahul.sharma@ayko.com>
2021-09-22 08:06:46 +0530

    first commit

---
+++ src/test/java/com/example/TitleOfYourFeatureTest.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.testng.annotations.*;
+
+public class TitleOfYourFeatureTest {
+    // Tags: tag
+    public Actionwords actionwords;
+    
+    @BeforeSuite
+    @Parameters({"browser"})
+    public void setUp(String browser) {
+        actionwords = new Actionwords(browser);
+    }
+
+    @Test
+    public void accessoriesPageNavigation() throws Exception {
+        // Given URL for Ribble Bikes works
+        actionwords.uRLForRibbleBikesWorks();
+        // When User hover over accessories in nav section
+        actionwords.userHoverOverAccessoriesInNavSection();
+        // And User clicks on Frameset
+        actionwords.userClicksOnFrameset();
+        // And User selects Price - Low to High
+        actionwords.userSelectsPriceLowToHigh();
+        // Then The price should be in acscending order
+        actionwords.thePriceShouldBeInAcscendingOrder();
+        // And User shuts down the browser
+        actionwords.userShutsDownTheBrowser();
+    }
+}
